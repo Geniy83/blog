fb9e135f1d9359dadc5117ed183a7e58af649fae
Geniy83 <3477734n@gmail.com>
2021-10-07 14:35:53 +0300

    Commit v1.3

--- src/main/java/com/evgeniy/blog/controllers/BlogController.java
+++ src/main/java/com/evgeniy/blog/controllers/BlogController.java
@@ -20,11 +20,9 @@ public class BlogController {
     private PostRepository postRepository;
 
     @GetMapping("/blog")
-    public String blogMain(@PathVariable("blog") String status, Model model) {
-        Optional<Post> post = postRepository.findByStatus(status);
-        ArrayList<Post> res = new ArrayList<>();
-        post.ifPresent(res :: add);
-        model.addAttribute("posts", res);
+    public String blogMain(Model model) {
+        Iterable<Post> posts = postRepository.findAll();
+        model.addAttribute("posts", posts);
         return "blog-main";
     }
 
